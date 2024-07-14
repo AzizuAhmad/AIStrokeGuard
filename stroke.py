f3b5dda1e99082b17c959dac9a4c913e112de917
Azizu Ahmad <azizu.rozaki@gmail.com>
2024-07-14 12:05:22 +0700

    secCommit

--- stroke.py
+++ stroke.py
@@ -1,8 +1,5 @@
-import itertools
 import pandas as pd
 import numpy as np
-from imblearn.over_sampling import SMOTE
-from sklearn.metrics import accuracy_score
 import streamlit as st
 import time
 import pickle
